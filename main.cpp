2822f17613127915b3b80db31a6ecab095463034
baltikaa9 <bm8av8df3t>
2022-12-09 11:16:33 +0300

    first commit

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <clocale>
+#include "std.h"
+
+using namespace std;
+
+//Программа является учебной
+
+int main() {
+    setlocale(LC_ALL, "rus");
+    
+    system("pause");
+    return 0;
+}
